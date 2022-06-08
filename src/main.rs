483f259b063bca5129d800bd34c9397d18a3a65d
Dennis <dennis@kobert.dev>
2022-06-08 18:55:33 +0200

    Fix dependencies

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,17 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::prelude::*;
-use embedded_hal::digital::v2::InputPin;
-use embedded_hal::digital::v2::OutputPin;
-use embedded_time::duration::Extensions;
+use embedded_hal::digital::v2::{InputPin, OutputPin};
 use embedded_time::rate::*;
-use embedded_time::Instant;
-use hal::clocks;
-use hal::spi::SpiDevice;
-use hal::timer::CountDown;
 use mfrc522::Mfrc522;
 use panic_halt as _;
-use rp_pico::entry;
 use rp2040_hal::spi::Spi;
-use rp_pico::hal;
-use rp_pico::hal::pac;
-use rp_pico::hal::prelude::*;
+use rp_pico::entry;
+use rp_pico::hal::{self, pac, prelude::*};
+use usbd_serial::SerialPort;
+
+// USB Device support
+use usb_device::{class_prelude::*, prelude::*};
 
 #[entry]
 fn main() -> ! {
@@ -52,26 +47,49 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let spi = Spi::<_,_,8>::new(pac.SPI0).init(&mut pac.RESETS, clocks.peripheral_clock.freq(), 1_000_000u32.Hz(), &embedded_hal::spi::MODE_0);
-    let mfrc = Mfrc522::new(spi, pins.gpio1.into_push_pull_output()).unwrap();
+    // Set up the USB driver
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
 
+    // Set up the USB Communications Class Device driver
+    let mut serial = SerialPort::new(&usb_bus);
 
-    if let Ok(atqa) = mfrc.reqa() {
-        if let Ok(uid) = mfrc.select(&atqa) {
-            
-            // TODO
-        }
-    }
+    // Create a USB device with a fake VID and PID
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(2) // from: https://www.usb.org/defined-class-codes
+        .build();
 
+    let spi = Spi::<_, _, 8>::new(pac.SPI0).init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        1_000_000u32.Hz(),
+        &embedded_hal::spi::MODE_0,
+    );
+    let mut mfrc = Mfrc522::new(spi, pins.gpio1.into_push_pull_output()).unwrap();
 
     let mut summer_pin = pins.gpio22.into_push_pull_output();
     let mut led = pins.led.into_push_pull_output();
     let mut door = pins.gpio28.into_pull_up_input();
-    let timer = 0u32;
+
     let mut state = State::Armed;
     const SUMMER_INTERVAL: u32 = 500;
 
     loop {
+        if let Ok(atqa) = mfrc.reqa() {
+            if let Ok(uid) = mfrc.select(&atqa) {
+                let uid_str = uid.as_bytes();
+                serial.write(uid_str).ok();
+            }
+        }
+
         state = match state {
             State::Armed => {
                 // TODO: Check RFID
@@ -114,7 +132,7 @@ fn main() -> ! {
                 } else {
                     State::Disarmed
                 }
-            },
+            }
         };
     }
 }
