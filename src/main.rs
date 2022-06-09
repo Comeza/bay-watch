83ef8f6e3f2787b3578d604a89efe21c4384f885
Dennis <dennis@kobert.dev>
2022-06-09 02:57:01 +0200

    whoooo working - gona rename commit  later sike

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-//! # Pico Blinky Example
-//!
 //! Blinks the LED on a Pico board.
 //!
 //! This will blink an LED attached to GP25, which is the pin the Pico uses for
@@ -12,6 +10,8 @@
 
 use embedded_hal::digital::v2::{InputPin, OutputPin};
 use embedded_time::rate::*;
+use hal::gpio::{FunctionSpi, Output, PushPull, Spi};
+use hal::spi::{Enabled, SpiDevice};
 use mfrc522::Mfrc522;
 use panic_halt as _;
 use rp2040_hal::spi::Spi;
@@ -20,13 +20,64 @@ use rp_pico::hal::{self, pac, prelude::*};
 use usbd_serial::SerialPort;
 
 // USB Device support
+use core::fmt::Write;
 use usb_device::{class_prelude::*, prelude::*};
+
+fn cards<'a>() -> [&'a [u8]; 11] {
+    let cards = [
+        [35, 188, 68, 167],
+        [243, 172, 67, 167],
+        [147, 204, 155, 167],
+        [227, 164, 122, 167],
+    ];
+    let transponders = [
+        [64, 107, 20, 27],
+        [144, 109, 125, 34],
+        [18, 95, 211, 27],
+        [97, 51, 50, 39],
+    ];
+    let aaron = [4, 29, 61, 74, 27, 92, 128];
+    let valle = [4, 27, 16, 74, 83, 102, 128];
+    let dennis = [4, 49, 21, 178, 51, 92, 128];
+    [
+        &[35, 188, 68, 167],
+        &[243, 172, 67, 167],
+        &[147, 204, 155, 167],
+        &[227, 164, 122, 167],
+        &[64, 107, 20, 27],
+        &[144, 109, 125, 34],
+        &[18, 95, 211, 27],
+        &[97, 51, 50, 39],
+        &[4, 29, 61, 74, 27, 92, 128],
+        &[4, 27, 16, 74, 83, 102, 128],
+        &[4, 49, 21, 178, 51, 92, 128],
+    ]
+}
+fn check_rfid<D: SpiDevice>(mfrc: Mfrc522<Spi<Enabled, D, 8>, Output<PushPull>>) -> bool {
+    if let Ok(atqa) = mfrc.reqa() {
+        if let Ok(uid) = mfrc.select(&atqa) {
+            let mut buf = [0u8; 64];
+            /*let _ = match write_to::show(&mut buf, format_args!("uid: {:?}\r\n", &uid.as_bytes())) {
+                Ok(uid) => serial.write(uid.as_bytes()),
+                Err(_) => serial.write(b"uid to long for buffer\r\n"),
+            };*/
+            return cards().iter().any(|id| id == &uid.as_bytes());
+        }
+    }
+    false
+}
 
 #[entry]
 fn main() -> ! {
+    // Grab our singleton objects
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
+
+    // Set up the watchdog driver - needed by the clock setup code
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    //
+    // The default is to generate a 125 MHz system clock
     let clocks = hal::clocks::init_clocks_and_plls(
         rp_pico::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
@@ -38,14 +89,6 @@ fn main() -> ! {
     )
     .ok()
     .unwrap();
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
-    let sio = hal::Sio::new(pac.SIO);
-    let pins = rp_pico::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
 
     // Set up the USB driver
     let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
@@ -56,8 +99,55 @@ fn main() -> ! {
         &mut pac.RESETS,
     ));
 
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins up according to their function on this particular board
+    let pins = rp_pico::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    use embedded_hal::spi::MODE_0;
+    use embedded_time::rate::*;
+    use rp2040_hal::{
+        gpio::{FunctionSpi, Pins},
+        pac,
+        spi::Spi,
+    };
+
+    let _ = pins.gpio2.into_mode::<FunctionSpi>();
+    let _ = pins.gpio3.into_mode::<FunctionSpi>();
+    let _ = pins.gpio4.into_mode::<FunctionSpi>();
+
+    let spi = Spi::<_, _, 8>::new(pac.SPI0).init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        1_000_000u32.Hz(),
+        &MODE_0,
+    );
+
+    /*let spi = Spi::<_, _, 8>::new(pac.SPI0).init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        1_000_000u32.Hz(),
+        &embedded_hal::spi::MODE_0,
+    );*/
+
+    let nss_pin = pins.gpio1.into_push_pull_output();
+    let mut mfrc = Mfrc522::new(spi, nss_pin).unwrap();
+
+    let mut summer_pin = pins.gpio22.into_push_pull_output();
+    let mut door = pins.gpio28.into_pull_up_input();
+    summer_pin.set_low();
+
+    let mut state = State::Armed;
+    const SUMMER_INTERVAL: u32 = 500;
     // Set up the USB Communications Class Device driver
     let mut serial = SerialPort::new(&usb_bus);
+    // Set the LED to be an output
+    let mut led = pins.led.into_push_pull_output();
 
     // Create a USB device with a fake VID and PID
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
@@ -67,73 +157,57 @@ fn main() -> ! {
         .device_class(2) // from: https://www.usb.org/defined-class-codes
         .build();
 
-    let spi = Spi::<_, _, 8>::new(pac.SPI0).init(
-        &mut pac.RESETS,
-        clocks.peripheral_clock.freq(),
-        1_000_000u32.Hz(),
-        &embedded_hal::spi::MODE_0,
-    );
-    let mut mfrc = Mfrc522::new(spi, pins.gpio1.into_push_pull_output()).unwrap();
+    //watchdog.start(1_050_000.microseconds());
 
-    let mut summer_pin = pins.gpio22.into_push_pull_output();
-    let mut led = pins.led.into_push_pull_output();
-    let mut door = pins.gpio28.into_pull_up_input();
-
-    let mut state = State::Armed;
-    const SUMMER_INTERVAL: u32 = 500;
-
+    let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+    let mut said_hello = false;
     loop {
-        if let Ok(atqa) = mfrc.reqa() {
-            if let Ok(uid) = mfrc.select(&atqa) {
-                let uid_str = uid.as_bytes();
-                serial.write(uid_str).ok();
-            }
+        // A welcome message at the beginning
+        if !said_hello && timer.get_counter() >= 10_000 {
+            said_hello = true;
+            let _ = serial.write(b"Test 6AcAb?\r\n");
         }
 
-        state = match state {
-            State::Armed => {
-                // TODO: Check RFID
+        //let _ = serial.write(b"no mfrc connected\r\n");
+        let _ = serial.flush();
 
-                if door.is_low().unwrap() {
-                    State::Alarming(0, false)
-                } else {
-                    state
+        // Check for new data
+        if usb_dev.poll(&mut [&mut serial]) {
+            let mut buf = [0u8; 64];
+            match serial.read(&mut buf) {
+                Err(_e) => {
+                    // Do nothing
                 }
-            }
-            State::Snoozed(timer) => {
-                // TODO: Check RFID
+                Ok(0) => {
+                    // Do nothing
+                }
+                Ok(count) => {
+                    // Convert to upper case
+                    buf.iter_mut().take(count).for_each(|b| {
+                        b.make_ascii_lowercase();
+                    });
 
-                if timer == 0 {
-                    State::Armed
-                } else {
-                    if door.is_low().unwrap() {
-                        State::Disarmed
-                    } else {
-                        State::Snoozed(timer - 1)
+                    if buf.contains(&b'1') {
+                        led.set_high().unwrap();
+                    } else if buf.contains(&b'0') {
+                        led.set_low().unwrap();
+                    }
+                    // Send back to the host
+                    let mut wr_ptr = &buf[..count];
+                    while !wr_ptr.is_empty() {
+                        match serial.write(wr_ptr) {
+                            Ok(len) => wr_ptr = &wr_ptr[len..],
+                            // On error, just drop unwritten data.
+                            // One possible error is Err(WouldBlock), meaning the USB
+                            // write buffer is full.
+                            Err(_) => break,
+                        };
                     }
                 }
             }
-            State::Alarming(timer, hl) => {
-                // TODO: Check RFID
-                if timer == 0 {
-                    if !hl {
-                        summer_pin.set_high();
-                    } else {
-                        summer_pin.set_low();
-                    };
-                    State::Alarming(SUMMER_INTERVAL, !hl)
-                } else {
-                    State::Alarming(timer - 1, hl)
-                }
-            }
-            State::Disarmed => {
-                if door.is_high().unwrap() {
-                    State::Armed
-                } else {
-                    State::Disarmed
-                }
-            }
-        };
+        }
+        mfrc.hlta();
+        mfrc.stop_crypto1();
     }
 }
 
@@ -142,4 +216,65 @@ enum State {
     Snoozed(u32),
     Alarming(u32, bool),
     Disarmed,
+}
+
+pub mod write_to {
+    use core::cmp::min;
+    use core::fmt;
+
+    pub struct WriteTo<'a> {
+        buffer: &'a mut [u8],
+        // on write error (i.e. not enough space in buffer) this grows beyond
+        // `buffer.len()`.
+        used: usize,
+    }
+
+    impl<'a> WriteTo<'a> {
+        pub fn new(buffer: &'a mut [u8]) -> Self {
+            WriteTo { buffer, used: 0 }
+        }
+
+        pub fn as_str(self) -> Option<&'a str> {
+            if self.used <= self.buffer.len() {
+                // only successful concats of str - must be a valid str.
+                use core::str::from_utf8_unchecked;
+                Some(unsafe { from_utf8_unchecked(&self.buffer[..self.used]) })
+            } else {
+                None
+            }
+        }
+    }
+
+    impl<'a> fmt::Write for WriteTo<'a> {
+        fn write_str(&mut self, s: &str) -> fmt::Result {
+            if self.used > self.buffer.len() {
+                return Err(fmt::Error);
+            }
+            let remaining_buf = &mut self.buffer[self.used..];
+            let raw_s = s.as_bytes();
+            let write_num = min(raw_s.len(), remaining_buf.len());
+            remaining_buf[..write_num].copy_from_slice(&raw_s[..write_num]);
+            self.used += raw_s.len();
+            if write_num < raw_s.len() {
+                Err(fmt::Error)
+            } else {
+                Ok(())
+            }
+        }
+    }
+
+    pub fn show<'a>(buffer: &'a mut [u8], args: fmt::Arguments) -> Result<&'a str, fmt::Error> {
+        let mut w = WriteTo::new(buffer);
+        fmt::write(&mut w, args)?;
+        w.as_str().ok_or(fmt::Error)
+    }
+}
+
+pub fn test() {
+    let mut buf = [0u8; 64];
+    let _s: &str = write_to::show(
+        &mut buf,
+        format_args!("write some stuff {:?}: {}", "foo", 42),
+    )
+    .unwrap();
 }
